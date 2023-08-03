2185f5db2d2f8731561673c807f7bfeaa2a9179d
Jair Murillo <brook_98@live.com>
2023-08-03 17:23:37 -0600

    feat:cchange color h3

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ console.log("Página en contruccion con Patricio Estrella");
 clickPatricio = () =>{
     alert("La mayones es un instrumento?")
 }
+
+changeColor = (elementHtml, color) =>{
+    
+    elementHtml.style.color = color
+
+}

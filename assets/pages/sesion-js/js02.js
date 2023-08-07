320becf8e8d7d3e43f47ae7887672d46f438ccfc
Jair Murillo <brook_98@live.com>
2023-08-07 15:39:33 -0600

    feat: intro funciones

--- assets/pages/sesion-js/js02.js
+++ assets/pages/sesion-js/js02.js
@@ -1,1 +1,11 @@
 console.log('Sesión JS-01');
+
+/*
+----------Funciones declaradas(function declaration, function statement)-----------
+
+Una caracteristica de las funciones delcaradas es que tienen un hoisting.
+hoisting: comportamiento en JS que permite a la declaración de variables o funciones, se eleven al comienzo de un ámbito antes de que se ejecute el código
+
+
+
+*/

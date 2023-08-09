ec2c115d47d1bc190248a6f3b622b7b48cea68b5
Jair Murillo <brook_98@live.com>
2023-08-09 17:28:58 -0600

    matriz ejemplo

--- assets/pages/sesion-js/js04.js
+++ assets/pages/sesion-js/js04.js
@@ -1,1 +1,30 @@
 console.log('Sesion js-04 matrices y bucles');
+
+//----Declamos un arreglo de 2 dimensiones(matriz).----
+/**
+ * Arrays anidados
+ * [ [],[],[] ]
+ */
+
+const personasEnCh30 = [ 
+    [ "Luis", "Allan" , "Anneth"  , "Maryluz" ] ,  /* ByteMe */
+    [ "Ed"  , "Jimena", "Marifer" , "Leo"    , "Alejandro" ] ,  /* PerryCode */
+    [ "Lu"  , "Leo"   , "Daniel"  , "Gina"    ]    /* BugBusters */
+];
+
+console.log(`Integrantes de BugBustes ${personasEnCh30[2]}`);
+console.log(`Integrantes de BugBustes ${personasEnCh30[2].join(', ')}`);
+console.log(`BugBustes integrante n.1 ${personasEnCh30[2][0]}`);
+
+//Hay que reemplazar el nombre de Leo en perryCode por Bryan
+
+personasEnCh30[1].splice(3, 1, 'Brayan')
+console.log(`${personasEnCh30[1]}`);
+
+for (let equipo = 0; equipo < personasEnCh30.length; equipo++) {
+    for (let persona = 0; persona < personasEnCh30[equipo].length; persona++) {
+        console.log(personasEnCh30[equipo][persona]);
+        
+    }
+    
+}

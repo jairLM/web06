c18dfa2a641c2c093e749c9344195be002ed4d31
Jair Murillo <brook_98@live.com>
2023-08-10 11:41:29 -0600

    break y continue

--- assets/pages/sesion-js/js04.js
+++ assets/pages/sesion-js/js04.js
@@ -42,11 +42,87 @@ for (let equipo = 0; equipo < personasEnCh30.length; equipo++) {
     console.log(letra);
   } 
 
-  for (const equipo of personasEnCh30){
+  //refactorizar funcion anterior con forEach
+  myPet.split('').forEach((letra)=>console.log(letra)); 
+
+
+
+ /* for (const equipo of personasEnCh30){
     console.log(equipo);
     for (const persona of equipo) {
         console.log(persona);
     }
-  }
+  }*/
 
   //---------forEach--------
+/*
+ Método que se utiliza para iterar colecciones, arreglos.
+ Perimite ejecutar una función por cada elemento del arreglo
+  
+ */
+ console.table(personasEnCh30);
+function iterarEquipos(equipo, indice, arreglo){
+    console.log(`Indice ${indice}: ${equipo}`);
+    equipo.forEach(iterarPersonas)
+    return equipo;
+}
+
+function iterarPersonas(persona, indice, arreglo){
+    console.log(`IndicePersona ${indice}: ${persona}`);
+    return persona;
+}
+
+ //personasEnCh30.forEach(iterarEquipos);
+
+personasEnCh30.forEach((equipo, indice)=> equipo.forEach((persona, indice)=>console.log(persona)));
+
+//---------uso de break------------
+
+/*for (let i = 0; i < 10; i++) {
+    if( i > 5 ) break;
+    console.log(i);    
+}*/
+
+// tabla de multiplicar
+/**
+ * 1 * 1 = 1
+ * 1 * 2 = 2
+ * 
+ */
+
+for (let i = 1; i <=5; i++) {
+    
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${j} x ${i} = ${j*i}`);
+        
+    }
+    
+}
+
+console.log('==============Uso de break============');
+rompeCicloSuperior:
+for (let i = 1; i <=5; i++) {
+    
+    for (let j = 1; j <= 10; j++) {
+        if(i === 2 && j === 5) break rompeCicloSuperior;
+        console.log(`${i} x ${j} = ${j*i}`);
+                
+    }
+    
+}
+
+//-------uso de continue-------
+/**
+ * Termina la ejecución en la iteración actual y continua con la proxima iteracion(o el tag que se indique)
+ */
+console.log('==============Uso de continue============');
+continuaSuperior:
+for (let i = 1; i <=5; i++) {
+    
+    for (let j = 1; j <= 10; j++) {
+        if(j > 3) continue continuaSuperior;
+        console.log(`${i} x ${j} = ${j*i}`);
+                
+    }
+    
+}

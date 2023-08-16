5e84c66e7bd2103c4a4b90f70e7936b30bb6baa6
Jair Murillo <brook_98@live.com>
2023-08-16 10:29:35 -0600

    ci: programacion secuencial

---
+++ assets/pages/sesion-js/js07.js
@@ -0,0 +1,16 @@
+console.log('JS-07 asincronía');
+
+
+const primerPaso =()=>{
+    console.log('01-inicio de mi programa');
+}
+const segundoPaso =()=>{
+    console.log('02-Desarrollp de mi programa');
+}
+const tercerPaso =()=>{
+    console.log('03-Fin de mi programa');
+}
+
+primerPaso();
+segundoPaso();
+tercerPaso();
